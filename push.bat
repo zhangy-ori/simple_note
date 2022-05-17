b61c2f5fcf5dfd4d2bfbc9a422157c43c437bde4
zhangyang_home <13338748020@163.com>
2022-05-17 21:40:00 +0800

    zhangyang_home于2022-05-date:0~8,2time:~0,2time:~3,2一键提交

--- push.bat
+++ push.bat
@@ -1,7 +1,6 @@
-
 @echo off
 
-set filename="zhangyang_home于%date:~0,4%-%date:~5,2%-%date: 8,2%    %time:~0,2%:%time:~3,2%一键提交"
+set filename="zhangyang_home于%date:~0,4%-%date:~5,2%-%date: ~8,2%  %time:~0,2%:%time:~3,2%一键提交"
 
 set "filename=%filename: =0%"
 
