c7af1f227912f8141700f3fa9b869bfdf5f1be8c
zhangyang_home <13338748020@163.com>
2022-05-18 21:18:06 +0800

    zhangyang_company于2022-05-18~21:18:06一键提交

--- push.bat
+++ push.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 
 set filename="zhangyang_company于%date:~0,4%-%date:~5,2%-%date:~8,2%~%time:~0,8%一键提交"
 
--- push_all.bat
+++ push_all.bat
@@ -1,8 +1,10 @@
 @echo on
+G:
+cd \Documents\gitee\chinaway
 set filename="zhangyang_home于%date:~0,4%-%date:~5,2%-%date:~8,2%~%time:~0,8%一键提交"
 set "filename=%filename: =0%"
 set "content=%filename%"
-set "branch=main"
+set "branch=master"
 set "file=."
 git add %file%
 git commit -m %content%
